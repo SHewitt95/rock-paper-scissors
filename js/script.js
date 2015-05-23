b49ce28a4223beb09ea0e76fee55ebe57a8d0288
Sherman Hewitt <s.hewitt7@umiami.edu>
2015-05-23 19:07:21 -0400

    Implemented logic for computer picking rock, paper or scissors.

--- js/script.js
+++ js/script.js
@@ -1,2 +1,28 @@
+"use strict";
+
 var playerChoice,
     compChoice;
+
+function pickChoice() {
+  // Uses Math.random to pick rock, paper or scissors.
+  var choice = Math.random();
+
+  if (choice <= 0.33) {
+
+    choice = 0; // 0 represents rock
+
+  } else if (choice >= 0.66) {
+
+    choice = 1; // 1 represents paper
+
+  } else {
+
+    choice = 2; // 2 represents scissors
+
+  }
+
+  return choice;
+}
+
+var number = pickChoice();
+console.log(number);
