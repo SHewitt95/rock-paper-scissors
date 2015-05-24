1815b8cc8d1f77708bdb19f98ca4e3c82a746679
Sherman Hewitt <s.hewitt7@umiami.edu>
2015-05-23 20:30:06 -0400

    Taking a break. While loops are complicated and dangerous...

--- js/script.js
+++ js/script.js
@@ -8,96 +8,95 @@ $(document).ready(function() {
       compScore = 0;
 
   var playerCards = document.getElementsByClassName("player-card");
-  var rock = document.getElementById("rock-card"),
-      paper = document.getElementById("paper-card"),
-      scissors = document.getElementById("scissors-card");
-
-  /* console.log(rock);
-  console.log(paper);
-  console.log(scissors);
-  console.log(typeof playerCards); */
-
-  function compChoice() {
-    // Uses Math.random to pick rock, paper or scissors.
-    var choice = Math.random();
-
-    if (choice <= 0.33) {
-
-      choice = "rock";
-
-    } else if (choice >= 0.66) {
-
-      choice = "paper";
-
-    } else {
-
-      choice = "scissors";
-
-    }
-
-    return choice;
-  }
-
-  function playerChoice() {
-    // Returns user's choice.
-
-  }
-
-  $(playerCards).click(function() {
-    console.log(this);
-  });
-
-  function updateScore(score) {
-    return score++;
-  }
 
   function game() {
 
-    while(playerScore < 5 && compScore < 5) {
-      var playerChoice = playerChoice(),
-          compChoice = compChoice();
+    function playerChoice() {
+      // Returns user's choice.
+      var choice;
 
-      if (playerChoice == "rock") { // First part of "if" block...
+      $(playerCards).click(function() {
+        choice = this.id;
+      });
 
-        if (compChoice == "rock") {
-          // Tie! No points given.
-        } else if (compChoice == "paper") {
-          // Computer gets a point.
-          updateScore(compScore);
-        } else if (compChoice == "scissors") {
-          // Player gets a point.
-          updateScore(playerScore);
-        }
+      return choice;
+    }
 
-      } else if (playerChoice == "paper") { // Second part of "if" block...
+    function compChoice() {
+      // Uses Math.random to pick rock, paper or scissors.
+      var choice = Math.random();
 
-        if (compChoice == "rock") {
-          // Player gets a point.
-          updateScore(playerScore);
-        } else if (compChoice == "paper") {
-          // Tie! No points given.
-        } else if (compChoice == "scissors") {
-          // Computer gets a point.
-          updateScore(compScore);
-        }
-
-      } else if (playerChoice == "scissors") { // Third part of "if" block...
-
-        if (compChoice == "rock") {
-          // Computer gets a point.
-          updateScore(compScore);
-        } else if (compChoice == "paper") {
-          // Player gets a point.
-          updateScore(playerScore);
-        } else if (compChoice == "scissors") {
-          // Tie! No points given.
-        }
-
+      if (choice <= 0.33) {
+        choice = "rock";
+      } else if (choice >= 0.66) {
+        choice = "paper";
+      } else {
+        choice = "scissors";
       }
+
+      return choice;
+    }
+
+    function updateScore(score) {
+      return score++;
+    }
+
+    if(playerScore < 5 && compScore < 5) {
+
+      var playerChoice,
+          compChoice;
+
+      $(playerCards).click(function() {
+        playerChoice = playerChoice();
+        compChoice = compChoice();
+
+        if (playerChoice == "rock") { // First part of "if" block...
+
+          if (compChoice == "rock") {
+            // Tie! No points given.
+          } else if (compChoice == "paper") {
+            // Computer gets a point.
+            updateScore(compScore);
+          } else if (compChoice == "scissors") {
+            // Player gets a point.
+            updateScore(playerScore);
+          }
+
+        } else if (playerChoice == "paper") { // Second part of "if" block...
+
+          if (compChoice == "rock") {
+            // Player gets a point.
+            updateScore(playerScore);
+          } else if (compChoice == "paper") {
+            // Tie! No points given.
+          } else if (compChoice == "scissors") {
+            // Computer gets a point.
+            updateScore(compScore);
+          }
+
+        } else if (playerChoice == "scissors") { // Third part of "if" block...
+
+          if (compChoice == "rock") {
+            // Computer gets a point.
+            updateScore(compScore);
+          } else if (compChoice == "paper") {
+            // Player gets a point.
+            updateScore(playerScore);
+          } else if (compChoice == "scissors") {
+            // Tie! No points given.
+          }
+
+        }
+
+        console.log("Player score: ", playerScore);
+        console.log("Computer score: ", compScore);
+
+      })
+
     }
 
   }
 
-  //game();
+  game();
 
 })
